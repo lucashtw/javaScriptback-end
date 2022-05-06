c228787e33eeb3017aaf958ba6dd41f681458176
lucas <lucashtw@gmail.com>
2022-05-06 16:45:07 -0300

    Tipos primitivos

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "ola, mundo!";
+const texto2 = 'ola, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// contatenação (+)
+
+console.log(citacao+meuNome);
+
+
+//template string ou template literal
+
