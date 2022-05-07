345c76d395d51a007f133489010ff36ef8a55044
lucas <lucashtw@gmail.com>
2022-05-07 09:43:38 -0300

    Declarando Variaveis

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+//tipo de dado
+// booleanos
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456";
+//const numeroString = Number("456");
+
+
+//console.log(numero === numeroString)
+
+//Nuymber()
+//Strubg()
+
+console.log(numero + Number(numeroString))
+
+//conversao explicita
---
+++ truthy-falsy.js
@@ -0,0 +1,18 @@
+//boolean
+const usuarioLogado = true
+const contapaga = false
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+let minhaVar;
+let varNull = null;
+
+
+let numero = 3;
+let texto = "alura";
+
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
