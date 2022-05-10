18f9066dd486a7f285579616f82cf34fc6a12c68
lucas <lucashtw@gmail.com>
2022-05-10 13:22:14 -0300

    Parametros e argumentos

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+//let x = "";
+//console.log(x);
+//x = "oi";
+
+function imprimeTexto(texto){
+    console.log(texto);
+} 
+
+imprimeTexto(soma());
+
+//imprimeTexto("outro texto");
+
+function soma(){
+    return 2+2;
+
+}
+
+//console.log(soma());
