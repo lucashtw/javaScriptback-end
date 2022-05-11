51d9752e0c7ed9b40941c7cf4c78f60d09ebbfaf
lucas <lucashtw@gmail.com>
2022-05-11 11:11:58 -0300

    funcoes finalizado

---
+++ expressoes-funcao.js
@@ -0,0 +1,21 @@
+//function minhaFuncao(param){
+    // bloco de codigo
+//}
+
+//minhaFuncao("param")
+
+// expressao de funcao
+
+console.log(soma(1,1))
+const soma = function(num1,num2){return num1 + num2}
+
+
+//diferença principal
+
+
+console.log(apresentar())
+
+function apresentar(){
+    return "olá"
+}
+
--- parametros.js
+++ parametros.js
@@ -23,8 +23,8 @@ function soma(numero1, numero2){
     return numero1+numero2;
 }
 
-function multiplica(numero1,numero2){
+function multiplica(numero1=1,numero2=1){
     return numero1 * numero2;
 }
 
-console.log(multiplica(soma(4,5),soma(3,3)))
+console.log(multiplica(soma(4, 5)))
